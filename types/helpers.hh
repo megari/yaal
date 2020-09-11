3a01a5fd0381c1f79efe05f4eff5db896376401f
Ari Sundholm <megari@iki.fi>
2020-09-11 20:34:38 +0300

    helpers.hh: Add a rudimentary template to simulate typeof().

--- types/helpers.hh
+++ types/helpers.hh
@@ -31,6 +31,87 @@ namespace yaal {
         template<bool b, typename ...Ts>
         using enable_if_t = typename enable_if<b, Ts...>::type;
 
+        // Typeof stuff.
+        template<typename T>
+        struct typeof_foo {
+            using type = T;
+            type val;
+            typeof_foo() {}
+        } __attribute__((packed));
+
+        template<typename T>
+        struct typeof_foo<T&> {
+            using type = typename typeof_foo<T>::type;
+            type val;
+            typeof_foo() {}
+        };
+
+        template<typename T>
+        struct typeof_foo<T&&> {
+            using type = typename typeof_foo<T>::type;
+            type val;
+            typeof_foo() {}
+        };
+
+        template<typename T>
+        constexpr bool nonref_type = true;
+        template<typename T>
+        constexpr bool nonref_type<T&> = false;
+        template<typename T>
+        constexpr bool nonref_type<T&&> = false;
+
+        template<typename T>
+        constexpr bool ref_type = !nonref_type<T>;
+
+        //template<typename T, enable_if_t<nonref_type<T>, T>* = nullptr>
+        //constexpr T typeof_func(T);
+        template<typename T /*, enable_if_t<nonref_type<T>, T>* = nullptr*/>
+        constexpr T typeof_func(T&);
+        template<typename T /*, enable_if_t<nonref_type<T>, T>* = nullptr*/>
+        constexpr T typeof_func(T&&);
+
+        template<typename T>
+        using typeof_t =
+            decltype(typeof_func(typeof_foo<T>().val));
+
+        template<typename T, typename S>
+        constexpr bool is_same_type = false;
+
+        template<typename T>
+        constexpr bool is_same_type<T, T> = true;
+
+        static_assert(is_same_type<int, int>, "Type mismatch. :(");
+        static_assert(is_same_type<typeof_t<int>, int>, "Type mismatch. :(");
+        static_assert(is_same_type<int, typeof_t<int>>, "Type mismatch. :(");
+        static_assert(is_same_type<typeof_t<int>, typeof_t<int>>,
+            "Type mismatch. :(");
+
+        static_assert(is_same_type<int&, int&>, "Type mismatch. :(");
+        static_assert(is_same_type<typeof_t<int&>, int>, "Type mismatch. :(");
+        static_assert(is_same_type<int, typeof_t<int&>>, "Type mismatch. :(");
+        static_assert(is_same_type<typeof_t<int&>, typeof_t<int>>,
+            "Type mismatch. :(");
+
+        static_assert(is_same_type<int&&, int&&>, "Type mismatch. :(");
+        static_assert(is_same_type<typeof_t<int&&>, int>, "Type mismatch. :(");
+        static_assert(is_same_type<int, typeof_t<int&&>>, "Type mismatch. :(");
+        static_assert(is_same_type<typeof_t<int&&>, typeof_t<int>>,
+            "Type mismatch. :(");
+
+        static_assert(is_same_type<typeof_t<int&&>, typeof_t<int&>>,
+            "Type mismatch. :(");
+        static_assert(is_same_type<typeof_t<int&>, typeof_t<int&&>>,
+            "Type mismatch. :(");
+
+        static_assert(!is_same_type<int, char>, "Bad type match. :(");
+        static_assert(!is_same_type<typeof_t<int>, char>, "Bad type match. :(");
+        namespace {
+            struct char_s { char c; };
+            static_assert(!is_same_type<char_s, char>,
+                "Bad type match. :(");
+            static_assert(is_same_type<typeof_t<decltype(char_s().c)>, char>,
+                "Type mismatch. :(");
+        }
     }
 
 }
